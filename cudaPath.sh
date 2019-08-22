e79369853bf971c16fec358634dad0f3e326227b
mingyen066 <mingyen066@gmail.com>
2019-08-22 16:42:44 +0800

    update

--- cudaPath.sh
+++ cudaPath.sh
@@ -1,5 +1,4 @@
 echo >> ~/.bashrc '
 export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64'
 source ~/.bashrc
