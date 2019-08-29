e6ad68403d85da464c96937f7862d0cd73bac906
mingyen066 <mingyen066@gmail.com>
2019-08-29 18:06:33 +0800

    update python3.7 -m pip install

--- python3Pip3.sh
+++ python3Pip3.sh
@@ -4,10 +4,11 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.7
 sudo apt-get install python3-pip
+python3.7 -m pip install upgrade pip
 sudo apt-get update
 
 echo >> ~/.bashrc 'alias python=python3.7'
-echo >> ~/.bashrc 'alias pip=pip3'
+echo >> ~/.bashrc 'alias pip=pip3.7'
 source ~/.bashrc
 echo "now you can use python3.7 and pip3 by directly typing python and pip repectively"
 
