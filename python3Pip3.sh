b1a9930a9ec951a877797b1f8fd57b8c0a1e1eed
Chung, Ming-Yen <mingyen066@gmail.com>
2019-10-16 17:06:53 +0800

    Update python3Pip3.sh

--- python3Pip3.sh
+++ python3Pip3.sh
@@ -4,8 +4,8 @@ sudo apt-get install git vim gcc make g++
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.7
-sudo python3.7 pip install pip
-sudo apt-get update
+sudo python3.7 -m pip install --upgrade pip
+  sudo apt-get update
 
 echo >> ~/.bashrc 'alias python=python3.7'
 echo >> ~/.bashrc 'alias pip=pip3.7'
